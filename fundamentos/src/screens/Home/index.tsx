0281aa11f0ca6fd7035372d6742771b115f63232
LuizFeix <luiz.2024014412@alunocesul.com>
2025-08-18 20:11:55 -0300

    newCommit.

--- fundamentos/src/screens/Home/index.tsx
+++ fundamentos/src/screens/Home/index.tsx
@@ -1,6 +1,8 @@
 import {FlatList, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native"
-import { Product } from "../components/Product";
 
+import { Product } from "../../components/Product";
+
+import { styles } from "./styles";
 
 export  function Home(){
     const products = [
@@ -89,70 +91,3 @@ export  function Home(){
             </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-
-        backgroundColor : "#EAEAEA",
-        padding: 24 
-    },
-    title: {
-        color: "#000",
-        fontSize: 24,
-        fontWeight: "bold",
-        lineHeight: 28.8,
-        marginTop: 48,
-
-    },
-    todaysDate: { 
-        color: "#000",
-        fontSize: 24,
-        fontWeight: "normal",
-        lineHeight: 28.8,
-    },
-    input: {
-        flex: 1,
-        height: 56,
-        padding: 16,
-        backgroundColor: "#FFF",
-        borderRadius: 5,
-        marginRight: 16,
-    },
-    form: {
-        width:"100%",
-        flexDirection: "row",
-        marginTop: 18,
-
-        marginBottom: 36
-    },
-    button: {
-        width: 56,
-        height: 56,
-        backgroundColor: "#31C667",
-        borderRadius: 5,
-
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    textButton: {
-        color: "#FFF",
-        fontSize: 24,
-    },
-    listTitle: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        lineHeight: 24,
-        marginBottom: 16,
-    },
-    listEmptyText: {
-        fontSize: 16,
-        fontWeight: 'normal',
-        lineHeight: 19.2,
-        textAlign: "center"
-    },
-    list: {
-        flex: 1,
-        justifyContent: 'center',
-    }
-});
